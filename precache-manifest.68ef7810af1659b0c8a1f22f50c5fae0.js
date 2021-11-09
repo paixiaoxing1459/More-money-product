ea45168e1cb0de884666c987f300c102ef06b8d1
lyric13579 <paixiaoxing1459@163.com>
2021-11-09 23:20:21 +0800

    deploy

---
+++ precache-manifest.68ef7810af1659b0c8a1f22f50c5fae0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe98ae5da5c781bfb90",
+    "url": "/More-money-product/css/app.fd34693e.css"
+  },
+  {
+    "revision": "c98fd7644c1b88164ce014eb416ef6c4",
+    "url": "/More-money-product/index.html"
+  },
+  {
+    "revision": "ebe98ae5da5c781bfb90",
+    "url": "/More-money-product/js/app.725d15e5.js"
+  },
+  {
+    "revision": "6c208a5afd86da798484",
+    "url": "/More-money-product/js/chunk-vendors.4aa92bde.js"
+  },
+  {
+    "revision": "42bfa4ba9edfb3426e53b3524294dc7f",
+    "url": "/More-money-product/manifest.json"
+  },
+  {
+    "revision": "9bcc11a22617f8ffa25b3b7c3c2a02b6",
+    "url": "/More-money-product/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/More-money-product/robots.txt"
+  }
+]);
